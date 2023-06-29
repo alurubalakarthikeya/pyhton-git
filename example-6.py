15b07b224daeea2971f95bea3eab4c10700aef48
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2023-06-29 10:41:11 +0530

    Add files via upload

---
+++ example-6.py
@@ -0,0 +1,8 @@
+#Introduction to elif statements which is else if in C lang
+num = eval(input("Enter a number: "))
+if num==0:
+    print("The number is zero")
+elif num>0:
+    print("the number is positive")
+else:
+    print("The number is negative")
