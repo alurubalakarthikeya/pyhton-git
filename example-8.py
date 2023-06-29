f033512b8e4ecf4972f1f45488d80a89de975dd6
Bala Karthikeya Aluru <91084020+alurubalakarthikeya@users.noreply.github.com>
2023-06-29 10:42:00 +0530

    Add files via upload

---
+++ example-8.py
@@ -0,0 +1,18 @@
+#using if else to find the number of week days
+week = eval(input("Enter a value: "))
+if week==1:
+    print("It's Sunday!")
+elif week==2:
+    print("It's Monday!")
+elif week==3:
+    print("It's Tuesday!")
+elif week==4:
+    print("It's Wednesday!")
+elif week==5:
+    print("It's Thursday!")
+elif week==6:
+    print("It's Friday!")
+elif week==7:
+    print("It's Saturday!")
+else: 
+    print("Invalid Input!")
